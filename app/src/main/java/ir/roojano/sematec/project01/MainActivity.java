a61e514d371d57bbe7c46f0f88f1d95c27ddef5f
unknown <malirezaei68@gmail.com>
2019-07-24 20:48:28 +0430

    task session3

--- app/src/main/java/ir/roojano/sematec/project01/MainActivity.java
+++ app/src/main/java/ir/roojano/sematec/project01/MainActivity.java
@@ -3,6 +3,7 @@ package ir.roojano.sematec.project01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("MyTag","OnCreate");
+
     }
+
+    @Override
+    protected void onStart() {
+        Log.d("MyTag","OnStart");
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d("MyTag","OnResume");
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d("MyTag","OnPause");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d("MyTag","OnStop");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d("MyTag","Destroy");
+        super.onDestroy();
+
+    }
+
+    @Override
+    protected void onRestart() {
+        Log.d("MyTag","OnRestart");
+        super.onRestart();
+        }
 }
+
